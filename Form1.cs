17acf0ec34116517d409d468c7766e54d2a80573
brilliant_sunshine <132028878+yesicandoanything@users.noreply.github.com>
2023-04-28 20:43:41 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -33,13 +33,10 @@ namespace pstFinder
                 {
                     process.Kill();
                 }
-                //MessageBox.Show("Outlook.exe 프로세스가 종료되었습니다.");
+           
             }
-            //else
-            //{
-            //    MessageBox.Show("Outlook.exe 프로세스가 실행 중이 아닙니다.");
-            //}
-
+            
+            //패치
             string registryPath = @"HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\office\16.0\outlook";
             string registryName = "disablepst";
             int registryValue = 0;
@@ -56,58 +53,8 @@ namespace pstFinder
 
             textBox1.Text = "패치 완료";
         }
-
-        //private void btnSearch_Click(object sender, EventArgs e)
-        //{
-        //    // 검색할 파일 확장자
-        //    string searchPattern = "*.pst";
-
-        //    // 검색할 드라이브 목록
-        //    string[] drives = new string[] { "C:", "D:" };
-
-        //    // 검색 결과 출력할 텍스트 박스 등
-        //    // ...
-
-        //    // 각 드라이브에서 파일 검색
-        //    foreach (string drive in drives)
-        //    {
-        //        try
-        //        {
-        //            DirectoryInfo di = new DirectoryInfo(drive);
-        //            FileInfo[] files = di.GetFiles(searchPattern, SearchOption.AllDirectories);
-        //            foreach (FileInfo file in files)
-        //            {
-        //                richTextBox1.AppendText(file.FullName + Environment.NewLine);
-        //                // 검색 결과 출력할 텍스트 박스 등에 결과 추가
-        //                // ...
-        //            }
-        //        }
-        //        catch (UnauthorizedAccessException)
-        //        {
-        //            //MessageBox.Show($"파일 검색 중 오류가 발생했습니다: {ex.Message}");
-        //        }
-        //    }
-
-        //    //string fileExtension = ".png";
-        //    //string[] drives = Environment.GetLogicalDrives();
-        //    //foreach (string drive in drives)
-        //    //{
-        //    //    try
-        //    //    {
-        //    //        DirectoryInfo dir = new DirectoryInfo(drive);
-        //    //        foreach (FileInfo file in dir.GetFiles($"*{fileExtension}", SearchOption.AllDirectories))
-        //    //        {
-        //    //            richTextBox1.AppendText(file.FullName + Environment.NewLine);
-        //    //        }
-        //    //    }
-        //    //    catch (UnauthorizedAccessException)
-        //    //    {
-        //    //        // 권한이 없는 경우 생략
-        //    //    }
-        //    //}
-
-        //}
-
+        
+        //
         private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
